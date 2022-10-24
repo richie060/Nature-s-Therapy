edfde8e472a86b9727ef148bdecee43745711f6a
richuhi060 <richuhi060@gmail.com>
2022-10-24 15:34:34 +0300

    first commit

---
+++ app/src/main/java/com/doctor/doctorsappointment/AskDoctorPatient.java
@@ -0,0 +1,38 @@
+package com.doctor.doctorsappointment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+import doctor.doctorsappointment.R;
+
+public class AskDoctorPatient extends AppCompatActivity {
+
+    private AppCompatButton doctor,patient;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ask_doctor_patient);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        doctor=findViewById(R.id.doctor);
+        patient=findViewById(R.id.patient);
+        doctor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AskDoctorPatient.this, DoctorRegister.class));
+                overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
+                finish();
+            }
+        });
+        patient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AskDoctorPatient.this,PatientLoginRegisterChoice.class));
+                overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/doctor/doctorsappointment/DataRetrievalClass/Doctor.java
@@ -0,0 +1,73 @@
+package com.doctor.doctorsappointment.DataRetrievalClass;
+
+public class Doctor {
+    private String Email,Address,City,FirstName,Id,LastName,MobileNo,Specialization;
+
+    public Doctor() {
+    }
+
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String city) {
+        City = city;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getMobileNo() {
+        return MobileNo;
+    }
+
+    public void setMobileNo(String mobileNo) {
+        MobileNo = mobileNo;
+    }
+
+    public String getSpecialization() {
+        return Specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        Specialization = specialization;
+    }
+}
